fd71d2457ff63090ff32a4bc0a9ca9bdc064cdb5
Estefac10 <estefac34@gmail.com>
2021-12-17 16:33:20 -0500

    cambio link

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import Profile from 'pages/profile';
 // import PrivateRoute from 'components/PrivateRoute';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://agile-bayou-92325.herokuapp.com',
 });
 
 const authLink = setContext((_, { headers }) => {
